4b88b1afd02963c58f781088bcf4a7cab2478380
hdlxt <839376636@qq.com>
2019-11-10 16:39:37 +0800

    '案例源码初始化'

---
+++ netty-study/src/main/java/com/imooc/netty/ch12/connection/Constant.java
@@ -0,0 +1,9 @@
+package com.imooc.netty.ch12.connection;
+
+/**
+ * @author
+ */
+public class Constant {
+    static final int BEGIN_PORT = 8000;
+    static final int N_PORT = 100;
+}
---
+++ netty-study/src/main/java/com/imooc/netty/ch12/thread/Constant.java
@@ -0,0 +1,8 @@
+package com.imooc.netty.ch12.thread;
+
+/**
+ * @author
+ */
+public class Constant {
+    public static final int PORT = 8000;
+}
